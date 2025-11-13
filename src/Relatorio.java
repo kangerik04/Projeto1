ea56263af806b8c0f03ebf02f1c029c2bd1af06a
kangerik04 <kangerik04@gmail.com>
2025-11-13 10:52:52 -0300

    Create Relatorio.java

---
+++ src/Relatorio.java
@@ -0,0 +1,10 @@
+public class Relatorio {
+
+    public void gerarPDF() {
+        System.out.println("Relatorio: gerando PDF...");
+    }
+
+    public void exibir() {
+        System.out.println("Relatorio: exibindo relatório.");
+    }
+}
