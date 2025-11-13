dab10283bcd1f1a66d57b8dc30657c24c6ece06d
kangerik04 <kangerik04@gmail.com>
2025-11-13 10:52:18 -0300

    Create DadosImagem.java

---
+++ src/DadosImagem.java
@@ -0,0 +1,7 @@
+public class DadosImagem implements IRegistroDados {
+
+    @Override
+    public void registrar() {
+        System.out.println("DadosImagem: salvando imagens da missão...");
+    }
+}
